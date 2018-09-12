59aceb91026a9211a298343156901be055ce6a99
Hizbullah Jamali <jam19e@pearcey-i2.cm.hpc>
2018-09-12 15:46:16 +1000

    updated progress notes and script file made executable

--- scripts/fasqc_single_file.sh
+++ scripts/fasqc_single_file.sh
@@ -1,4 +1,4 @@
-#hhhh
+#!/bin/bash
 
 # To run fastqc on a single file
 
@@ -10,5 +10,6 @@ fastqc
 
 # Now use this command to run fasqc on single file by specifyimg the file path to Bowen where this file is stored
 
-fastqc /OSM/CBR/AF_DATASCHOOL/input/Hiz_Kim/data/Pool1_S1_R1_001.fastq.gz > processed/fasqc/
-	
+fastqc /OSM/CBR/AF_DATASCHOOL/input/Hiz_Kim/data/Pool1_S1_R1_001.fastq.gz > /OSM/CBR/AF_DATASCHOOL/input/Hiz_Kim/processed/fastqc/Pool1_S1_R1_001_fastqc.html 
+
+# Adding script after > redirects the oupt fiel to a specific directory

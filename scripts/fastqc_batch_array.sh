a30b4cc473c2e46e05e19467588036004970ce0b
Hizbullah Jamali <jam19e@pearcey-i2.cm.hpc>
2018-09-14 10:48:08 +1000

    created logs folder to redirect slurm.out files there

--- scripts/fastqc_batch_array.sh
+++ scripts/fastqc_batch_array.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#BATCH --job-name=fastqc
+#SBATCH --job-name=fastqc
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=2GB
 #SBATCH --array=0-24
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hiz.jamali@csiro.au
-
+#SBATCH --output=logs/fastqc_%A.out
 
 #-------------- set input and output directories---------#
  
@@ -18,7 +18,7 @@ OUT_DIR=/OSM/CBR/AF_DATASCHOOL/input/Hiz_Kim/processed/fastqc/
 
 module load fastqc
 
-IN_FILE_LIST=( $(cut -d , -f 1 outputs/file_list.csv ) );
+IN_FILE_LIST=( $(cut -d , -f 1 outputs/file_list.csv ) ); #can also use cat 
 
 if [ ! -z "$SLURM_ARRAY_TASK_ID" ]
     then
---
+++ scripts/file_list.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+#---------------------------------------------------------#
+# This script lists the file names from /OSM/CBR/AF_DATASCHOOL/input/Hiz_Kim/data/
+# in a .csv file
+#---------------------------------------------------------#
+
+
+ls /OSM/CBR/AF_DATASCHOOL/input/Hiz_Kim/data/ > outputs/file_list.csv
+
